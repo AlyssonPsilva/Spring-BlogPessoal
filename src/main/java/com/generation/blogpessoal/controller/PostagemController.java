22676ae3eb8e4f9b5d0bd04f68193523487b9c8f
Alysson Pereira <jose.apereira@mercadolivre.com>
2022-11-24 20:49:15 -0300

    implementado método put.

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -36,4 +36,9 @@ public class PostagemController {
     public ResponseEntity <Postagem> post(@Valid @RequestBody Postagem postagem){
         return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
     }
+    @PutMapping
+    public ResponseEntity <Postagem> put(@Valid @RequestBody Postagem postagem){
+        return postagemRepository.findById(postagem.getId()).map(resposta ->ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem)))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
 }

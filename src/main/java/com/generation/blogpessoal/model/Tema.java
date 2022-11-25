9febc74cc2e296122b92ac06088ca5ac069b6ee4
Alysson Pereira <jose.apereira@mercadolivre.com>
2022-11-25 19:50:09 -0300

    criada estrutura base da classe Tema.

---
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -0,0 +1,32 @@
+package com.generation.blogpessoal.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_temas")
+public class Tema {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "O Atributo Descrição é obrigatório.")
+    private String descricao;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}

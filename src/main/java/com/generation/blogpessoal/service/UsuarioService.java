33c4dc1732a281d73eeb3a2d657a02609e8c8767
Alysson Pereira <jose.apereira@mercadolivre.com>
2022-12-02 15:06:43 -0300

    criada interface usuário repository.

--- src/main/java/com/generation/blogpessoal/service/UsuarioService.java
+++ src/main/java/com/generation/blogpessoal/service/UsuarioService.java
@@ -1,5 +1,6 @@
 package com.generation.blogpessoal.service;
 
+import com.generation.blogpessoal.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 

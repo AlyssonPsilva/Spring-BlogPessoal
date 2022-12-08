604bcf79158a43dad4033d578f5bc1b368d9ece9
Alysson Pereira <jose.apereira@mercadolivre.com>
2022-12-08 17:50:35 -0300

    implementado os testes

--- src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
@@ -12,4 +12,5 @@ import java.util.Optional;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     public Optional<Usuario> findByUsuario(String usuario);
+    public List <Usuario> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 }
